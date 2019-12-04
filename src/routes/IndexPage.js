205dcc8f434dbc7cc7f25d327665615ba5f2b5b1
Aaron <1132413764@qq.com>
2019-12-04 10:59:51 +0800

    bugfix

---
+++ src/routes/IndexPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'dva';
+import styles from './IndexPage.css';
+import Layout from 'antd'
+
+
+function IndexPage() {
+  return (
+    <div>123</div>
+  );
+}
+
+IndexPage.propTypes = {
+};
+
+export default connect()(IndexPage);
