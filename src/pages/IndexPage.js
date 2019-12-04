e03e6b570bcd971c0b4fd5b4dd49fcc6263da563
Aaron <1132413764@qq.com>
2019-12-05 00:53:32 +0800

    导航

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.scss';
-import {Layout} from 'antd'
-const {Header,Content}=Layout
+import NavBar from './NavBar'
+import { Layout } from 'antd'
+const { Header, Content } = Layout
 
 function IndexPage() {
   return (
     <Layout className={styles.layout}>
-    	<Header className={styles.header}>123</Header>
-    	<Content className={styles.content}>456</Content>
+      <Header className={styles.header}>
+        <NavBar />
+      </Header>
+      <Content className={styles.content}>456</Content>
     </Layout>
   );
 }
